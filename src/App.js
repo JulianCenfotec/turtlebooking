f21a8dc3bd9c35b37a2e50bf0c03bd7bbcacf98b
JulianCenfotec <jcabreram@ucenfotec.ac.cr>
2023-11-19 12:33:54 -0600

    Primer avance
    Hola Mundo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState(''); // Estado para el valor del input
+  const [saludo, setSaludo] = useState('Hola Mundo'); // Estado para el saludo
+
+  // Manejar el cambio en el input
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Manejar el clic en el botón
+  const handleButtonClick = () => {
+    // Actualizar el saludo con el valor del input
+    setSaludo(`Hola ${inputValue || 'Mundo'}`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="Hola-Mundo">{saludo}</p>
+
+        {/* Añadir un campo de entrada de texto (input) */}
+        <input
+          type="text"
+          placeholder="Dime tu nombre"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+         <div style={{ margin: '10px' }}></div>
+        {/* Añadir un botón con el evento onClick */}
+        <button onClick={handleButtonClick}>Oprimeme</button>
       </header>
     </div>
   );
