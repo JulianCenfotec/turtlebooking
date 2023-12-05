d13c1857cfc73dc04e429c9e76fbff74b3e7a191
Fabrizio Badilla Lee <fbadillal@ucenfotec.ac.cr>
2023-12-04 19:46:07 -0600

    Cambios de Hotel y Restaurant

--- src/components/forms/HotelReservation.jsx
+++ src/components/forms/HotelReservation.jsx
@@ -5,6 +5,7 @@ import {
     Textarea,
     FormControl,
     FormLabel,
+    Button,
     Box
 } from '@chakra-ui/react'
 
@@ -13,26 +14,27 @@ const HotelReservation = () => {
         <Box p={4}>
             <h1>Hotel Reservation</h1>
             <FormControl id="check-in-date" my={4}>
-                <FormLabel>Fecha de check-in</FormLabel>
+                <FormLabel>Check-in date</FormLabel>
                 <Input type="date" />
             </FormControl>
 
             <FormControl id="check-out-date" my={4}>
-                <FormLabel>Fecha de check-out</FormLabel>
+                <FormLabel>Check-out date</FormLabel>
                 <Input type="date" />
             </FormControl>
 
             <FormControl id="number-of-people" my={4}>
-                <FormLabel>Numero de personas</FormLabel>
+                <FormLabel>Number of people</FormLabel>
                 <NumberInput min={1}>
                     <NumberInputField />
                 </NumberInput>
             </FormControl>
 
             <FormControl id="room-preferences" my={4}>
-                <FormLabel>Preferencias de habitacion</FormLabel>
+                <FormLabel>Room preferences</FormLabel>
                 <Textarea placeholder="Ingresa aqui tus preferencias para la habitacion" />
             </FormControl>
+            <Button colorScheme="blue" mt={4}>Reserve</Button>
         </Box>
     )
 }
